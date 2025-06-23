520028b6f1fef09ae1c3c303346c0f098bb31431
Дарья <karazanova@bk.ru>
2025-06-23 16:44:44 +0300

    Исправлена ошибка в адресе

--- fixture/soap.py
+++ fixture/soap.py
@@ -10,7 +10,8 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.27.1/api/soap/mantisconnect.php?wsdl")
+        web_config = self.app.config['web']
+        client = Client(f"{web_config['baseUrl']}/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
